5a457cc321b623e46d4b87803d6a2a6cb9588679
streeg <streeg@github.com>
2019-04-18 12:22:00 -0300

    write in file

--- trabalho1oac.asm
+++ trabalho1oac.asm
@@ -62,36 +62,30 @@
 #        J:  | OP  |           TARGET / ADDRESS           |
 #        */   
 
-.data
-entrada_asm: .asciiz "example_saida.asm"
-buffer: .space 1024
+        .data
+fout:   .asciiz "testout.txt"      # filename for output
+buffer: .asciiz "The quick brown fox jumps over the lazy dog."
+        .text
 
 
-.text
-
-
-# Open file for reading
-
-li   $v0, 13          # system call for open file
-la   $a0, entrada_asm      # input file name
-li   $a1, 0           # flag for reading
-li   $a2, 0           # mode is ignored
-syscall               # open a file 
-move $s0, $v0         # save the file descriptor  
-
-
-# reading from file just opened
-
-li   $v0, 14        # system call for reading from file
-move $a0, $s0       # file descriptor 
-la   $a1, buffer    # address of buffer from which to read
-li   $a2,  11       # hardcoded buffer length
-syscall             # read from file
-
-# Printing File Content
-li  $v0, 4          # system Call for PRINT STRING
-la  $a0, buffer     # buffer contains the values
-syscall             # print int
-
-li $v0, 10      # Finish the Program
-syscall
+  ###############################################################
+  # Open (for writing) a file that does not exist
+  li   $v0, 13       # system call for open file
+  la   $a0, fout     # output file name
+  li   $a1, 1        # Open for writing (flags are 0: read, 1: write)
+  li   $a2, 0        # mode is ignored
+  syscall            # open a file (file descriptor returned in $v0)
+  move $s6, $v0      # save the file descriptor 
+  ###############################################################
+  # Write to file just opened
+  li   $v0, 15       # system call for write to file
+  move $a0, $s6      # file descriptor 
+  la   $a1, buffer   # address of buffer from which to write
+  li   $a2, 44       # hardcoded buffer length
+  syscall            # write to file
+  ###############################################################
+  # Close the file 
+  li   $v0, 16       # system call for close file
+  move $a0, $s6      # file descriptor to close
+  syscall            # close file
+  ###############################################################
